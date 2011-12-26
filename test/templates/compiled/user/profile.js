00452f934553aef2a9322613df4f022645cc1a9d
Daniel Stocks <daniel@webcloud.se>
2011-12-26 18:57:47 +0100

    updated test files

--- test/templates/compiled/user/profile.js
+++ test/templates/compiled/user/profile.js
@@ -1,1 +1,2 @@
-<h1>{{name}}</h1>
+JST["/user/profile"] = new HoganTemplate();
+JST["/user/profile"].r = function(cx,p){var c = [cx];var b = "";var _ = this;b += "<h1>";b += (_.v(_.f("name",c,p,0)));b += "</h1>";b += "\n";return b;;};
